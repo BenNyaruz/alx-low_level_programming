3fabe53e937a0155098dba87424290d67c5838a7
BenNyaruz <bennyaruviro@gmail.com>
2022-06-03 20:57:03 -0700

    preprocessor

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -13,7 +12,7 @@ int main(void)
 	long along;
 	long long alonglong;
 	float afloat;
-
+	
 	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
 	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(along));

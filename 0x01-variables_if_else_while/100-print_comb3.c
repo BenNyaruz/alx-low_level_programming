0255fbccddd1fe9c2aa4245fa5aa492cbbda954c
BenNyaruz <bennyaruviro@gmail.com>
2022-06-04 14:23:29 -0700

    variables

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,24 +7,23 @@
 */
 int main(void)
 {
-  int tens;
-  int ones;
+	int tens;
+	int ones;
+	for (tens = 0; tens <= 9; tens++)
+    	{
+      		for (ones = tens + 1; ones <= 9; ones++)
+		{
+	  		putchar(tens + '0');
+	  		putchar(ones + '0');
 
-  for (tens = 0; tens <= 9; tens++)
-    {
-      for (ones = tens + 1; ones <= 9; ones++)
-	{
-	  putchar(tens + '0');
-	  putchar(ones + '0');
+	  		if (tens < 8)
+	   		 {
+	     			 putchar(',');
+	      			 putchar(' ');
+	    		 }
+		}
+    	}
+  	putchar('\n');
 
-	  if (tens < 8)
-	    {
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-    }
-  putchar('\n');
-
-  return (0);
+ 	return (0);
 }
--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,6 +10,10 @@ int main(void)
 	int digit;
 
 	for (digit = 0; digit < 10; digit++)
-	printf("%i", digit);
+	{
+		printf("%i", digit);
+	}
+
+	printf("\n");
 	return (0);
 }
